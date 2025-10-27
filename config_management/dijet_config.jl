c70f1bdda7f806be492a32d95d2fb58915eb247b
angelo <52296518+Angelo92Git@users.noreply.github.com>
2025-10-27 19:55:52 -0400

    config: add dijet config

---
+++ config_management/dijet_config.jl
@@ -0,0 +1,43 @@
+pow2(x) = x^2
+pow3(x) = x^3
+pow4(x) = x^4
+pow5(x) = x^5
+
+const CONFIG_sr = Dict(
+    "binary_operators" => [+, -, *, /],
+    "unary_operators" => [pow2, pow3, exp, log],
+    "constraints" => [],
+    # "constraints" => [exp => 4, log => 4],
+    "nested_constraints" => [pow2 => [pow2 => 0], pow3 => [pow3 => 0], exp => [exp => 0], log => [log => 0]],
+    "maxsize" => 30,
+    "ncycles_per_iteration" => 380,
+    "parsimony" => 0.0,
+    "warmup_maxsize_by" => 0.0,
+    "adaptive_parsimony_scaling" => 1040,
+    "parallelism_for_marginal_sr" => :multithreading,
+    "parallelism_for_conditional_sr" => :multithreading,
+    "parallelism_for_joint_sr" => :multithreading,
+    "niterations_for_marginal_sr" => 4000,
+    "niterations_for_conditional_sr" => 4000,
+    "niterations_for_joint_sr" => 8000,
+    "num_populations_for_marginal_sr" => 15,
+    "num_populations_for_conditional_sr" => 15,
+    "num_populations_for_joint_sr" => 15,
+    "population_size_for_marginal_sr" => 30,
+    "population_size_for_conditional_sr" => 30,
+    "population_size_for_joint_sr" => 30,
+    "joint_expression_possibilities" => "cartesian", # "one_to_one" or "cartesian"    
+    "joint_max_num_expressions_per_dim_and_slice" => 30, # use Inf to avoid limiting growth in number of expressions per dim and slice when multiplying conditionals and marginals
+    "joint_max_num_expressions" => 450, # use Inf to avoid limiting number of expressions
+    "batching" => true,
+    "batch_size" => 128,
+    "progress" => false,
+    "verbosity" => false,
+    "joint_use_frequency" => true,
+    "joint_use_frequency_in_tournament" => true,
+    "turbo" => false, # Does not work here
+    "downsample_joint_data" => true,
+    "num_samples_joint_data" => 1000, # Number of samples to use for joint data downsampling
+    "elementwise_loss" => LossFunctions.L2DistLoss(),
+    "scale_density" => true
+)
