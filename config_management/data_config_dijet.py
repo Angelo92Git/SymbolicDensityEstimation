f9a49ba10e9c53ac50bd0b8e1aa81516e344722f
angelo <52296518+Angelo92Git@users.noreply.github.com>
2025-12-21 13:49:18 -0500

    config: add density range scaling to dijet data config

--- config_management/data_config_dijet.py
+++ config_management/data_config_dijet.py
@@ -4,7 +4,7 @@ class DataConfig:
     processed_data_prefix = "dijet_minmax_scaled"
     columns = ['mjj', 'HT']
     jxbins = 10000j # Number of bins for joint distribution
-    bw_adj_joint = 0.3 # Bandwidth adjustment for joint distribution KDE #120 # Old value
+    bw_adj_joint_range = (0.2, 0.4)
     kernel_type = 'gaussian' # Kernel type for KDE
     slices=None
     density_range_scaling_target=None
