eed10fc5ddff7f7c671ad5ed7ee70b326c799810
angelo <52296518+Angelo92Git@users.noreply.github.com>
2025-12-25 18:29:54 -0500

    miscellaneous

--- gis34_runcode.sh
+++ gis34_runcode.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=logs/slurm-%j-%N_gis34.out      # the print of xxx.jl will be logged in this file, %N for node name, %j for job id:w
 
 module load StdEnv/2023 julia/1.11.3
-julia --thread=64 sdes_pipeline.jl "gaussian_independent_set_34" "config_gaussian" "false"
+julia --thread=64 sdes_pipeline.jl "gaussian_independent_set34" "config_gaussian" "false"
 
 # module load python/3.12
 # module load scipy-stack
