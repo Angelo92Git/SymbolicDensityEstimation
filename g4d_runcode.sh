44964c53e644776d130bf8dcd52116270881cef3
angelo <52296518+Angelo92Git@users.noreply.github.com>
2025-12-25 15:13:06 -0500

    add gaussian 4d script

---
+++ g4d_runcode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=g4d                             # Job name
+#SBATCH --time=0-16:00:00                         # Time limit hrs:min:sec
+#SBATCH --ntasks=1                                # Number of tasks (processes)
+#SBATCH --cpus-per-task=30                        # Number of CPU cores per task (maximum on compute canada is 50)
+#SBATCH --mem=128G                                 # Memory per node
+#SBATCH --output=logs/slurm-%j-%N_g4d.out          # the print of xxx.jl will be logged in this file, %N for node name, %j for job id:w
+
+module load StdEnv/2023 julia/1.11.3
+julia --thread=64 sdes_pipeline.jl "gaussian_4d" "config_gaussian" "false"
+
+# module load python/3.12
+# module load scipy-stack
+# pip install KDEpy
+# pip install --upgrade KDEpy
+# srun --cpus-per-task=1 --mem=25G --time=01:00:00 --pty bash
+## on fir
+# srun --partition=cpupreempt --cpus-per-task=1 --mem=32G --time=01:00:00 --pty bash
