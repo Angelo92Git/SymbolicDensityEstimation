485936ec1674746026a14b15d52765b63d30ba3d
angelo <52296518+Angelo92Git@users.noreply.github.com>
2026-01-03 13:45:38 -0500

    scripts: add gm_data_gen

---
+++ gm_data_gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=gm_data                            # Job name
+#SBATCH --time=0-4:00:00                              # Time limit hrs:min:sec
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks=1                                    # Number of tasks (processes)
+#SBATCH --cpus-per-task=4                             # Number of CPU cores per task (maximum on compute canada is 50)
+#SBATCH --mem-per-cpu=2G                              # Memory per node
+#SBATCH --output=logs/slurm-%j-%N_gm_data.out         # the print of xxx.jl will be logged in this file, %N for node name, %j for job id:w
+
+module load python/3.12
+module load scipy-stack
+source ~/torch_env/bin/activate
+python -m data_processing_scripts.gen_data data_config_2d_gaussian_mixture
