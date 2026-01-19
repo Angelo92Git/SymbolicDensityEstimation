33d40c012ee8e0ec492604b0ea476d17d6e80881
angelo <52296518+Angelo92Git@users.noreply.github.com>
2026-01-18 20:50:56 -0500

    scripts: add dijet pure and hybrid

--- dijet_pure_runcode.sh
+++ dijet_pure_runcode.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=dijet_nde                        # Job name
+#SBATCH --job-name=dijet_pur                        # Job name
 #SBATCH --time=0-16:00:00                           # Time limit hrs:min:sec
 #SBATCH --ntasks=1                                  # Number of tasks (processes)
 #SBATCH --cpus-per-task=25                          # Number of CPU cores per task (maximum on compute canada is 50)
 #SBATCH --mem=128G                                  # Memory per node
-#SBATCH --output=logs/slurm-%j-%N_dijet_nde.out     # the print of xxx.jl will be logged in this file, %N for node name, %j for job id:w
+#SBATCH --output=logs/slurm-%j-%N_dijet_pur.out     # the print of xxx.jl will be logged in this file, %N for node name, %j for job id:w
 
 module load StdEnv/2023 julia/1.11.3
 julia --thread=64 sdes_pipeline.jl "dijet_neural" "config_dijet_pureloss" "false"
